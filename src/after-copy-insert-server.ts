31832059c0eb2cf2c510c93036d0ee8d1a157231
Tim Perry <pimterry@gmail.com>
2019-01-09 12:20:59 +0100

    Lock the specific server version used, for direct control of upgrades & reproducibility

--- src/after-copy-insert-server.ts
+++ src/after-copy-insert-server.ts
@@ -1,42 +1,49 @@
+import * as _ from 'lodash';
 import { promisify } from 'util';
 import * as path from 'path';
 import * as fs from 'fs';
 import fetch from 'node-fetch';
 
+const packageJsonLock = require('../package-lock.json');
+
 const targz = require('targz');
 const extractTarGz = promisify(targz.decompress);
 const deleteFile = promisify(fs.unlink);
 
+const serverVersion: string = 'v' + packageJsonLock.dependencies['httptoolkit-server'].version;
+
 /*
-* Download the latest server binary and include it in the build directly.
-* We use the binary build rather than just depending on this through npm
+* Download the correct server binary and include it in the build directly.
+* We use the binary build rather than our actual npm dev dependency
 * because the binary builds are capable of autoupdating all by themselves,
 * and because it makes it possible for users to run the server directly
 * with minimal effort, if they so choose.
 */
 export = async function (buildPath: string, _electronVersion: string, platform: string, arch: string, callback: Function) {
     try {
-        console.log(`Downloading httptoolkit-server for ${platform}-${arch}`);
+        console.log(`Downloading httptoolkit-server ${serverVersion} for ${platform}-${arch}`);
 
-        const serverVersion = new RegExp(`httptoolkit-server-v\\d+\\.\\d+\\.\\d+-${platform}-${arch}.tar.gz`);
+        const assetRegex = new RegExp(`httptoolkit-server-${serverVersion}-${platform}-${arch}.tar.gz`);
 
         const headers = process.env.GITHUB_TOKEN ? { Authorization: `token ${process.env.GITHUB_TOKEN}` } : {}
 
         const response = await fetch(
-            'https://api.github.com/repos/httptoolkit/httptoolkit-server/releases/latest',
+            'https://api.github.com/repos/httptoolkit/httptoolkit-server/releases',
             { headers }
         );
 
-        const release = await response.json();
+        const releases = await response.json();
+
+        const release = _.find(releases, { tag_name: serverVersion });
+
         if (!release || !release.assets) {
             console.error(JSON.stringify(release, null, 2));
             throw new Error('Could not retrieve release assets');
         }
 
-        const asset = release.assets.filter((asset: { name: string }) => asset.name.match(serverVersion))[0];
-
+        const asset = release.assets.filter((asset: { name: string }) => asset.name.match(assetRegex))[0];
         if (!asset) {
-            throw new Error(`No server available matching ${serverVersion.toString()}`);
+            throw new Error(`No server available matching ${assetRegex.toString()}`);
         }
 
         console.log(`Downloading server from ${asset.browser_download_url}...`);
