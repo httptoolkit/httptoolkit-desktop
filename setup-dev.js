a5c50a206372fdd01d9e1259b83d34a1152ab902
Tim Perry <pimterry@gmail.com>
2019-01-09 12:36:03 +0100

    Redownload the server in dev if the required version has changed

--- setup-dev.js
+++ setup-dev.js
@@ -3,17 +3,26 @@ require('ts-node/register');
 const { promisify } = require('util');
 const os = require('os');
 const fs = require('fs');
+const rimraf = require('rimraf');
+const insertServer = promisify(require('./src/after-copy-insert-server'));
+
 const canAccess = (file) => promisify(fs.access)(file).then(() => true).catch(() => false);
+const deleteDir = promisify(rimraf);
 
+const packageJsonLock = require('./package-lock.json');
+const requiredServerVersion = packageJsonLock.dependencies['httptoolkit-server'].version;
+
+// Manually trigger the after-copy hook, to give us an env like the real package
 async function setUpDevEnv() {
-    // Manually trigger the after-copy hook, to give us an env like the real package
-    const afterCopy = promisify(require('./src/after-copy-insert-server'));
+    const serverExists = await canAccess('./httptoolkit-server');
+    const serverVersion = serverExists ? require('./httptoolkit-server/package.json').version : null;
 
-    if (!await canAccess('./httptoolkit-server')) {
-        await afterCopy(__dirname, '', os.platform(), os.arch());
+    if (serverVersion !== requiredServerVersion) {
+        if (serverExists) await deleteDir('./httptoolkit-server');
+        await insertServer(__dirname, '', os.platform(), os.arch());
         console.log('Dev setup completed.');
     } else {
-        console.log('Server already downloaded, nothing to do.');
+        console.log('Correct server already downloaded, nothing to do.');
     }
 }
 
