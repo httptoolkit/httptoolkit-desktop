f4c02b6ccc61cb42a9b0631d1ed191aa93027152
Evan Rolfe <evanrolfe@gmail.com>
2019-12-19 13:39:30 +0000

    Fix npm start:dev when httptoolkit-server dir does not exist

--- setup-dev.js
+++ setup-dev.js
@@ -2,12 +2,8 @@ require('ts-node/register');
 
 const { promisify } = require('util');
 const path = require('path');
-const os = require('os');
 const fs = require('fs');
-const rimraf = require('rimraf');
 
-const canAccess = (file) => promisify(fs.access)(file).then(() => true).catch(() => false);
-const deleteDir = promisify(rimraf);
 const mkdir = promisify(fs.mkdir);
 const writeFile = promisify(fs.writeFile);
 const chmod = promisify(fs.chmod);
@@ -20,20 +16,14 @@ setInterval(() => {}, 999999999);
 // This lets us edit both and the desktop together. We do this by creating a fake server,
 // which doesn't exit, but otherwise does nothing.
 async function setUpDevEnv() {
-    const serverFolder = path.join(__dirname, 'httptoolkit-server');
-    const serverExists = await canAccess(serverFolder);
-    if (serverExists) {
-        await deleteDir(serverFolder);
+    const serverBinFolder = path.join(__dirname, 'httptoolkit-server', 'bin');
+    await mkdir(serverBinFolder, { recursive: true });
 
-        const binFolder = path.join(serverFolder, 'bin');
-        await mkdir(binFolder, { recursive: true });
-
-        const bins = ['httptoolkit-server', 'httptoolkit-server.cmd'].map((bin) => path.join(binFolder, bin));
-        await Promise.all(bins.map(async (bin) => {
-            await writeFile(bin, sleepForeverScript);
-            await chmod(bin, 0o755);
-        }));
-    }
+    const bins = ['httptoolkit-server', 'httptoolkit-server.cmd'].map((bin) => path.join(serverBinFolder, bin));
+    await Promise.all(bins.map(async (bin) => {
+        await writeFile(bin, sleepForeverScript);
+        await chmod(bin, 0o755);
+    }));
 }
 
 setUpDevEnv().catch(e => {
