0f4153ba02848176eee5bbe5329b886188e9acce
Evan Rolfe <accounts@evanrolfe.me>
2019-12-20 09:58:38 +0000

    Setup-dev: create the dummy bin files even if the server folder doesnt exist

--- setup-dev.js
+++ setup-dev.js
@@ -2,8 +2,12 @@ require('ts-node/register');
 
 const { promisify } = require('util');
 const path = require('path');
+const os = require('os');
 const fs = require('fs');
+const rimraf = require('rimraf');
 
+const canAccess = (file) => promisify(fs.access)(file).then(() => true).catch(() => false);
+const deleteDir = promisify(rimraf);
 const mkdir = promisify(fs.mkdir);
 const writeFile = promisify(fs.writeFile);
 const chmod = promisify(fs.chmod);
@@ -16,10 +20,15 @@ setInterval(() => {}, 999999999);
 // This lets us edit both and the desktop together. We do this by creating a fake server,
 // which doesn't exit, but otherwise does nothing.
 async function setUpDevEnv() {
-    const serverBinFolder = path.join(__dirname, 'httptoolkit-server', 'bin');
-    await mkdir(serverBinFolder, { recursive: true });
+    const serverFolder = path.join(__dirname, 'httptoolkit-server');
+    const serverExists = await canAccess(serverFolder);
 
-    const bins = ['httptoolkit-server', 'httptoolkit-server.cmd'].map((bin) => path.join(serverBinFolder, bin));
+    if (serverExists) await deleteDir(serverFolder);
+
+    const binFolder = path.join(serverFolder, 'bin');
+    await mkdir(binFolder, { recursive: true });
+
+    const bins = ['httptoolkit-server', 'httptoolkit-server.cmd'].map((bin) => path.join(binFolder, bin));
     await Promise.all(bins.map(async (bin) => {
         await writeFile(bin, sleepForeverScript);
         await chmod(bin, 0o755);
