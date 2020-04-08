7b6f55b7eeaf6997a44150407d2744485888b5e5
Tim Perry <pimterry@gmail.com>
2020-04-08 14:03:39 +0200

    Delete installed servers on startup if the bundled server is newer
    This reduces incompatibility risk, and resolves some bugs with users
    updating between current desktop versions.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import * as Sentry from '@sentry/electron';
 Sentry.init({ dsn: 'https://1194b128453942ed9470d49a74c35992@sentry.io/1367048' });
 
 function reportError(error: Error | string) {
+    console.log(error);
+
     if (typeof error === 'string') {
         Sentry.captureMessage(error);
     } else {
@@ -12,12 +14,15 @@ function reportError(error: Error | string) {
 
 import { spawn, exec, ChildProcess } from 'child_process';
 import * as os from 'os';
+import { promises as fs } from 'fs'
 import * as path from 'path';
+import { promisify } from 'util';
 import * as querystring from 'querystring';
 import { app, BrowserWindow, shell, Menu, dialog } from 'electron';
 import * as uuid from 'uuid/v4';
 import * as yargs from 'yargs';
-
+import * as semver from 'semver';
+import * as rimraf from 'rimraf';
 import * as windowStateKeeper from 'electron-window-state';
 
 import registerContextMenu = require('electron-context-menu');
@@ -28,13 +33,17 @@ registerContextMenu({
 import { reportStartupEvents } from './report-install-event';
 import { menu } from './menu';
 
+const rmRF = promisify(rimraf);
+
 const packageJson = require('../package.json');
+const packageJsonLock = require('../package-lock.json');
 
 const isWindows = os.platform() === 'win32';
 
 const APP_URL = process.env.APP_URL || 'https://app.httptoolkit.tech';
 const AUTH_TOKEN = uuid();
 const DESKTOP_VERSION = packageJson.version;
+const BUNDLED_SERVER_VERSION = packageJsonLock.dependencies['httptoolkit-server'].version;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -230,6 +239,65 @@ if (!amMainInstance) {
         dialog.showErrorBox(title, body);
     }
 
+    // On startup, we want to kill server directories if the bundled server version is newer. This ensures that
+    // the desktop app can always guarantee the server is at least its bundled version, to automatically pick
+    // up updates more quickly and avoid incompatibilities.
+    async function cleanupOldServers() {
+        // This mirrors the path logic from Oclif:
+        // https://github.com/oclif/config/blob/master/src/config.ts +
+        // https://github.com/oclif/plugin-update/blob/master/src/hooks/init.ts
+
+        const homeDir = process.env.HOME ||
+            (isWindows && (
+                (
+                    process.env.HOMEDRIVE &&
+                    process.env.HOMEPATH &&
+                    path.join(process.env.HOMEDRIVE!, process.env.HOMEPATH!)
+                ) ||
+                process.env.USERPROFILE
+            )) ||
+            os.homedir() ||
+            os.tmpdir();
+
+        const oclifDataPath = path.join(
+            process.env.XDG_DATA_HOME ||
+            (isWindows && process.env.LOCALAPPDATA) ||
+            path.join(homeDir, '.local', 'share'),
+            'httptoolkit-server'
+        );
+
+        const serverUpdatesPath = process.env.OCLIF_CLIENT_HOME ||
+            path.join(oclifDataPath, 'client');
+
+        const serverPaths = await fs.readdir(serverUpdatesPath);
+
+        if (serverPaths.some((filename) =>
+            !semver.valid(filename.replace(/\.partial\.\d+$/, '')) &&
+            filename !== 'bin' &&
+            filename !== 'current'
+        )) {
+            // If the folder contains something other than the expected version folders, be careful.
+            console.log(serverPaths);
+            reportError(
+                `Server path (${serverUpdatesPath}) contains unexpected content, ignoring`
+            );
+            return;
+        }
+
+        // If the bundled server is newer than all installed server versions, then
+        // delete all the installed server versions entirely before we start.
+        if (!serverPaths.some((serverPath) => {
+            try {
+                return semver.gt(serverPath, BUNDLED_SERVER_VERSION)
+            } catch (e) {
+                return false;
+            }
+        })) {
+            console.log('All server versions installed are outdated, deleting');
+            await rmRF(serverUpdatesPath);
+        }
+    }
+
     async function startServer(retries = 2) {
         const binName = isWindows ? 'httptoolkit-server.cmd' : 'httptoolkit-server';
         const serverBinPath = path.join(__dirname, '..', 'httptoolkit-server', 'bin', binName);
@@ -323,7 +391,9 @@ if (!amMainInstance) {
         // Brief delay before quitting, so our analytics register
         setTimeout(() => app.quit(), 500);
     } else {
-        startServer().catch((err) => {
+        cleanupOldServers().catch(console.log)
+        .then(() => startServer())
+        .catch((err) => {
             console.error('Failed to start server, exiting.', err);
 
             // Hide immediately, shutdown entirely after a brief pause for Sentry
