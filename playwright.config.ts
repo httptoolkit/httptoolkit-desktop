6d5eed38943cbb7415a696436b68b5a8d35b08c1
Tim Perry <pimterry@gmail.com>
2025-11-21 12:56:54 +0100

    Increase test timeout for flaky CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
     testDir: './test',
-    timeout: 20000,
+    timeout: 30000,
     workers: 1,
     retries: 0,
     reporter: 'list',
